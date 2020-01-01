baa28dc0e96459f6689711a43395884c79b9e2a6
wangxinji111 <18515121213@163.com>
2020-01-02 01:47:15 +0800

    添加注释

--- springboot/src/main/java/com/wxj/app/App.java
+++ springboot/src/main/java/com/wxj/app/App.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.wxj.controller")
 @EnableAutoConfiguration
 public class App {
+    //先用扫包的启动方式
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
     }
